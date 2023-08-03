e568a39b5c7af2055f1a4a20fc9c98a4f5eecc27
yassinouk <yassin.ouakka@etu.uae.ac.ma>
2023-08-03 20:48:58 +0100

    test: adding arg to func TestCreateAccount, and testing using testify/require.

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,26 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "yassin",
+		Balance:  1000000,
+		Currency: "USD",
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
 
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 }
