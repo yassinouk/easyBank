c63bf5356b24605e26d3b92a161f91891eda39f9
yassinouk <yassin.ouakka@etu.uae.ac.ma>
2023-08-07 12:34:55 +0100

    changes: changing the schema of the database (account-->accounts)

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -74,11 +74,11 @@ func TestListAccounts(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		CreateRandomAccount(t)
 	}
-	arg := ListAccountParams{
+	arg := ListAccountsParams{
 		Limit:  5,
 		Offset: 5,
 	}
-	accounts, err := testQueries.ListAccount(context.Background(), arg)
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
 	require.Len(t, accounts, 5)
 	for _, account := range accounts {
